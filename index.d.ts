c478988a91d7994768a46a760d1c6836e2075f4c
Joshua Jacobowitz <joshua.jacobowitz@yahoo.de>
2023-03-06 13:34:28 -0500

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+/// <reference no-default-lib="true"/>
+
+/// <reference lib="es2022" />
+/// <reference lib="esnext.intl" />
+/// <reference types="@total-typescript/ts-reset/dist/fetch" />
+/// <reference types="@total-typescript/ts-reset/dist/filter-boolean" />
+/// <reference types="@total-typescript/ts-reset/dist/is-array" />
+/// <reference types="@total-typescript/ts-reset/dist/json-parse" />
+/// <reference types="@total-typescript/ts-reset/dist/array-includes" />
+/// <reference types="@total-typescript/ts-reset/dist/set-has" />
